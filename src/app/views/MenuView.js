bef2881b871798db7a4e8525bd5a820bc0580c74
vikkis <baheryan.vika@gmail.com>
2016-04-10 13:30:01 +0300

    Add menu

--- src/app/views/MenuView.js
+++ src/app/views/MenuView.js
@@ -25,6 +25,8 @@ export default class MenuView {
               return(`<li class="menu-list-item"><a href=${item.link} class=${item.iconClass}>${item.title}</a></li>`)
             }).join('')}
           </ul>
+
+          <div class="menu-logout">Log out</div>
         </div>
 
 			</div>
