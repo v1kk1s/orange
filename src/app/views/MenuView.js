93675e5b2e89f224cd77d7328c64e4e9cd40b7eb
vikkis <baheryan.vika@gmail.com>
2016-04-10 13:10:41 +0300

    Add menu items

--- src/app/views/MenuView.js
+++ src/app/views/MenuView.js
@@ -1,21 +1,31 @@
+import MenuItems from '../models/menuItems.js';
 
 export default class MenuView {
   constructor (container) {
     this.container = container;
     this.content = null;
+    this.menuItems = new MenuItems().menuItems;
   }
 
 
   render () {
     this.content = `
 			<div class="menu">
+
         <div class="menu-avatar-wrap">
           <div class="menu-user-info">
             <div class="menu-avatar"></div>
             <p class="menu-name">Kate Mitty</p>
           </div>
         </div>
-        
+
+        <div class="menu-list-wrap">
+          <ul>
+            ${this.menuItems.map((item) => {
+              return(`<li class="menu-list-item"><a href=${item.link} class=${item.iconClass}>${item.title}</a></li>`)
+            }).join('')}
+          </ul>
+        </div>
 
 			</div>
 		`;
