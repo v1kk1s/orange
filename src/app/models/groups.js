15c87dccadffd533b12e2dff36987280c3a3f02e
v1kk1s <baheryan.vika@gmail.com>
2016-06-21 15:11:13 +0300

    Add groups model

---
+++ src/app/models/groups.js
@@ -0,0 +1,15 @@
+export default class GroupsModel {
+
+  constructor() {
+    this.groups = [
+      {
+        name: 'friends',
+        members: ['Chandler', 'Chandler', 'Chandler']
+      },
+      {
+        name: 'favourite',
+        members: ['Rachel Green', 'Rachel Green', 'Rachel Green', 'Rachel Green']
+      }
+    ];
+  }
+}
--- src/app/views/GroupsView.js
+++ src/app/views/GroupsView.js
@@ -1,10 +1,21 @@
-import ContactsModel from '../models/contacts';
+import GroupsModel from '../models/groups';
 
 export default class GroupsView {
   constructor (container) {
     this.container = container;
     this.content = null;
-    this.contacts = new ContactsModel().contacts;
+    this.groups = new GroupsModel().groups;
+  }
+
+  getGroups(group) {
+    return `
+      <div class="groups-item">
+        <div class="groups-img default">
+          <div class="groups-counter">${group.members.length}</div>
+        </div>
+        <div class="groups-name">${group.name}</div>
+      </div>
+    `
   }
 
   render () {
@@ -15,12 +26,7 @@ export default class GroupsView {
 
         <div class="groups list-page">
 
-          <div class="groups-item">
-            <div class="groups-img default">
-              <div class="groups-counter">4</div>
-            </div>
-            <div class="groups-name">Friends</div>
-          </div>
+          ${this.groups.map(this.getGroups.bind(this)).join('')}
 
         </div>
 			</div>
