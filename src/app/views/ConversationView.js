25c8fdfc073f3588a6179e51142b397307b24883
vikkis <baheryan.vika@gmail.com>
2016-08-13 18:29:41 +0300

    Show not found msg if message history is empty

--- src/app/views/ConversationView.js
+++ src/app/views/ConversationView.js
@@ -1,42 +1,52 @@
-
+import ContactsModel from '../models/contacts';
 
 export default class ConversationView {
   constructor (container) {
     this.container = container;
     this.content = null;
+    this.contacts = new ContactsModel().contacts;
+  }
+
+  getIcon(id) {
+    return this.contacts.filter((contact) => {
+      return contact.id == id;
+    });
   }
 
 
   render (messages) {
+
     console.log(messages);
-
-    this.content = `
+    this.content =  messages.length ? `
 			<div class="convers list-page">
+        ${messages.map((msg) => {
+          if(msg.userId == 999) {
+            return (`
+                  <div class="convers-wrap convers-me">
+                    <div class="convers-msg">${msg.message}</div>
+                    <div class="convers-date">${msg.time.getHours()}:${msg.time.getMinutes()}</div>
+                  </div>
+                `);
+          } else {
+            let icon = this.getIcon(msg.userId)[0].icon;
+            return (`
+                  <div class="convers-wrap convers-other">
+                    <img src="../public/img/${icon}" class="convers-img"/img>
+                    <div class="convers-msg">${msg.message}</div>
+                    <div class="convers-date">${msg.time.getHours()}:${msg.time.getMinutes()}</div>
+                  </div>
+                `);
+          }
 
-      ${messages.map((msg) => {
-
-        if(msg.userId == 999) {
-          return `
-            <div class="convers-wrap convers-me">
-              <div class="convers-msg">${msg.message}</div>
-              <div class="convers-date">${msg.time.getHours()}:${msg.time.getMinutes()}</div>
-            </div>
-          `;
-        } else {
-          return `
-            <div class="convers-wrap convers-other">
-              <div class="convers-img"></div>
-              <div class="convers-msg">${msg.message}</div>
-              <div class="convers-date">${msg.time.getHours()}:${msg.time.getMinutes()}</div>
-            </div>
-          `;
-        }
-
-      })}
-
-
-
+    }).join('')}
 			</div>
+		` : `
+      <div class="convers list-page">
+        <div class="convers-none convers-msg">
+            No messages in your conversation history :( <br/>
+            Start messaging now!
+         </div>
+      </div>
 		`;
 
     this.container.insertAdjacentHTML('beforeend', this.content );
